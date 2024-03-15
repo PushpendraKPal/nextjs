bff1ba439f808f19e8af1cdcbf01b25daa8533a0
PushpendraKPal <kumarpushp1992@gmail.com>
2024-03-15 16:07:21 +0530

    interection of pages

--- news_app/app/about/page.tsx
+++ news_app/app/about/page.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link";
+
 function aboutUs() {
+  const details: { id: string; name: string; role: string }[] = [
+    { id: "1", name: "Yash", role: "Senior Developer" },
+    { id: "2", name: "Vaibhav", role: "Backend Developer" },
+    { id: "3", name: "Suresh", role: "Frontend Developer" },
+  ];
   return (
     <div className="text-center mt-8 text-2xl">
-      Hello NextJS, <br />
+      Hello Developers, <br />
       <br />
-      AboutUs Page
+      {details.map((e) => {
+        return (
+          <div>
+            <Link href={`/about/${e.id}`}>{e.name}</Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
