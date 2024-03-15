e9e56401117c096c08b18cad582e279e181e5d1d
PushpendraKPal <kumarpushp1992@gmail.com>
2024-03-15 12:44:49 +0530

    basics of NextJs

---
+++ news_app/app/about/page.tsx
@@ -0,0 +1,11 @@
+function aboutUs() {
+  return (
+    <div className="text-center mt-8 text-2xl">
+      Hello NextJS, <br />
+      <br />
+      AboutUs Page
+    </div>
+  );
+}
+
+export default aboutUs;
